bd42dc82c15c1f51af81f6ab1d5f6f6b53203e3d
hodgs <hodgsonjamie@hotmail.com>
2024-07-23 12:30:23 +0100

    Refactored LearnerService & added tests

--- Ncfe.CodeTest/LearnerService.cs
+++ Ncfe.CodeTest/LearnerService.cs
@@ -1,9 +1,6 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.Configuration;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace Ncfe.CodeTest
 {
@@ -11,61 +8,48 @@ namespace Ncfe.CodeTest
     {
         public Learner GetLearner(int learnerId, bool isLearnerArchived)
         {
-            Learner archivedLearner = null;
+            LearnerResponse learnerResponse = null;
 
-            if (isLearnerArchived)
+            if (!isLearnerArchived)
             {
-                var archivedDataService = new ArchivedDataService();
-                archivedLearner = archivedDataService.GetArchivedLearner(learnerId);
-
-                return archivedLearner;
-            }
-            else
-            {
-
-                var failoverRespository = new FailoverRepository();
-                var failoverEntries = failoverRespository.GetFailOverEntries();
-
-
-                var failedRequests = 0;
-
-                foreach (var failoverEntry in failoverEntries)
-                {
-                    if (failoverEntry.DateTime > DateTime.Now.AddMinutes(-10))
-                    {
-                        failedRequests++;
-                    }
-                }
-
-                LearnerResponse learnerResponse = null;
-                Learner learner = null;
-
-                if (failedRequests > 100 && (ConfigurationManager.AppSettings["IsFailoverModeEnabled"] == "true" || ConfigurationManager.AppSettings["IsFailoverModeEnabled"] == "True"))
+                //Failover mode logic moved to private method
+                if (IsInFailoverMode())
                 {
                     learnerResponse = FailoverLearnerDataAccess.GetLearnerById(learnerId);
                 }
                 else
                 {
-                    var dataAccess = new LearnerDataAccess();
-                    learnerResponse = dataAccess.LoadLearner(learnerId);
-
-
+                    learnerResponse = new LearnerDataAccess().LoadLearner(learnerId);
                 }
+            }
 
-                if (learnerResponse.IsArchived)
-                {
-                    var archivedDataService = new ArchivedDataService();
-                    learner = archivedDataService.GetArchivedLearner(learnerId);
-                }
-                else
-                {
-                    learner = learnerResponse.Learner;
-                }
-
-
-                return learner;
+            //Rearranged logic to use one call to GetArchivedLearner for both use cases
+            if (isLearnerArchived || learnerResponse.IsArchived)
+            {
+                return new ArchivedDataService().GetArchivedLearner(learnerId);
+            }
+            else
+            {
+                return learnerResponse.Learner;
             }
         }
 
+        private bool IsInFailoverMode()
+        {
+            //No need to query DB if the setting is not true
+            if (bool.Parse(ConfigurationManager.AppSettings["IsFailoverModeEnabled"]))
+            {
+                var failoverTimeStart = DateTime.Now.AddMinutes(-10);
+
+                var failedRequestCount = new FailoverRepository()
+                    .GetFailOverEntries()
+                    .Where(foe => foe.DateTime > failoverTimeStart)
+                    .Count();
+
+                return failedRequestCount > 100;
+            }
+
+            return false;
+        }
     }
 }
---
+++ Test/GetLearnerFromFailoverLearnerDataAccessTests.cs
@@ -0,0 +1,101 @@
+﻿using System.Configuration;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Ncfe.CodeTest;
+
+namespace Test
+{
+    [TestClass]
+    public class GetLearnerFromFailoverLearnerDataAccessTests
+    {
+        private LearnerService learnerService;
+
+        [TestInitialize]
+        public void Setup()
+        {
+            learnerService = new LearnerService();
+
+            //TEST DATA - Add 99 FailoverEntries with a DateTime set in the last 10 mins
+            //TEST DATA - Add a Learner with an Id of 1 to FailoverLearnerDataAccess
+        }
+
+        [TestMethod]
+        //isLearnerArchived = false
+        //IsFailoverModeEnabled = true
+        //100 FailoverEntries exist
+        //LearnerResponse.IsArchived = false
+        public void Test001_AssertNotNull()
+        {
+            ConfigurationManager.AppSettings.Set("IsFailoverModeEnabled", "true");
+            //TEST DATA - Add 1 FailoverEntry with a DateTime set in the last 10 mins
+
+            var result = learnerService
+                .GetLearner(1, false);
+
+            Assert.IsNotNull(result);
+        }
+
+        [TestMethod]
+        //isLearnerArchived = true
+        //IsFailoverModeEnabled = true
+        //100 FailoverEntries exist
+        //LearnerResponse.IsArchived = false
+        public void Test002_AssertNull()
+        {
+            ConfigurationManager.AppSettings.Set("IsFailoverModeEnabled", "true");
+            //TEST DATA - Add 1 FailoverEntry with a DateTime set in the last 10 mins
+
+            var result = learnerService
+                .GetLearner(1, true);
+
+            Assert.IsNull(result);
+        }
+
+        [TestMethod]
+        //isLearnerArchived = false
+        //IsFailoverModeEnabled = false
+        //100 FailoverEntries exist
+        //LearnerResponse.IsArchived = false
+        public void Test003_AssertNull()
+        {
+            ConfigurationManager.AppSettings.Set("IsFailoverModeEnabled", "false");
+            //TEST DATA - Add 1 FailoverEntry with a DateTime set in the last 10 mins
+
+            var result = learnerService
+                .GetLearner(1, false);
+
+            Assert.IsNull(result);
+        }
+
+        [TestMethod]
+        //isLearnerArchived = false
+        //IsFailoverModeEnabled = true
+        //100 FailoverEntries do not exist
+        //LearnerResponse.IsArchived = false
+        public void Test004_AssertNull()
+        {
+            ConfigurationManager.AppSettings.Set("IsFailoverModeEnabled", "true");
+
+            var result = learnerService
+                .GetLearner(1, false);
+
+            Assert.IsNull(result);
+        }
+
+        [TestMethod]
+        //isLearnerArchived = false
+        //IsFailoverModeEnabled = true
+        //100 FailoverEntries exist
+        //LearnerResponse.IsArchived = true
+        public void Test005_AssertNull()
+        {
+            ConfigurationManager.AppSettings.Set("IsFailoverModeEnabled", "true");
+            //TEST DATA - Add 1 FailoverEntry with a DateTime set in the last 10 mins
+            //TEST DATA - Set Learner(Id:1) Archived = true
+
+            var result = learnerService
+                .GetLearner(1, false);
+
+            Assert.IsNull(result);
+        }
+    }
+}
